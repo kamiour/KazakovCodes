94fbe46ca684540f5ea5ace5530020b68b856497
Mykhailo Kazakov <kamiournew@gmail.com>
2019-09-09 21:26:40 +0100

    updated hero size

--- src/js/main.js
+++ src/js/main.js
@@ -4,13 +4,13 @@
 //= ../../bower_components/jquery/dist/jquery.js
 
 $(document).ready(function(){
-    //= components/swipers.js
-    //= components/burger.js
-    //= components/button-expand.js
-    //= components/nav-item.js
-    //= components/hero-button.js
-    //= components/tab.js
-  })
+  //= components/swipers.js
+  //= components/burger.js
+  //= components/button-expand.js
+  //= components/nav-item.js
+  //= components/hero-button.js
+  //= components/tab.js
+})
 
 /*
  * Custom
